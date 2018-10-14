5fcb1ccdf0eaef18c18f1b5ab4cf8aa9dd233953
Yoran Brondsema <yoran@sutori.com>
2018-10-14 12:17:52 -0500

    Make tests pass

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ module.exports = {
        * We intentionally use the semantically not quite correct `didPrepare` hook instead of `upload` to work around
        * an issue with ember-cli-deploy-gzip (and other compression plugins like -brotli or -compress), where gzipped
        * sourcemaps are uploaded that Sentry is not able to decompress automatically. By using a hook before `willUpload`
-       * we will upload the still uncomspressed files.
+       * we will upload the still uncompressed files.
        *
        * See https://github.com/dschmidt/ember-cli-deploy-sentry/issues/26 and https://github.com/getsentry/sentry/issues/4566
        */
--- tests/unit/index-nodetest.js
+++ tests/unit/index-nodetest.js
@@ -56,7 +56,7 @@ describe('deploySentry plugin', function() {
 
     assert.equal(typeof plugin.configure, 'function');
     assert.equal(typeof plugin.prepare, 'function');
-    assert.equal(typeof plugin.upload, 'function');
+    assert.equal(typeof plugin.didPrepare, 'function');
     assert.equal(typeof plugin.didDeploy, 'function');
   });
 
@@ -121,7 +121,7 @@ describe('deploySentry plugin', function() {
           return previous;
         }, []);
 
-        assert.equal(messages.length, 4);
+        assert.equal(messages.length, 5);
       });
 
       it('adds default config to the config object', function() {
@@ -144,6 +144,7 @@ describe('deploySentry plugin', function() {
         context.config.deploySentry["filePattern"] = "/**/*.{js,map}";
         context.config.deploySentry["enableRevisionTagging"] = false;
         context.config.deploySentry["replaceFiles"] = true;
+        context.config.deploySentry["strictSSL"] = true;
       });
 
       it('does not warn about missing optional config', function() {
@@ -156,6 +157,7 @@ describe('deploySentry plugin', function() {
 
           return previous;
         }, []);
+
         assert.equal(messages.length, 0);
       });
     });
@@ -166,10 +168,10 @@ describe('deploySentry plugin', function() {
       assert.equal(typeof subject.contentFor, 'function');
     });
     it('returns content for head-footer', function() {
-      assert.equal(subject.contentFor('head-footer'), '<meta name="sentry:revision"></meta>');
+      assert.equal(subject.contentFor('head-footer'), '<meta name="sentry:revision">');
     });
     it('does not return content for other types', function() {
-      assert.notEqual(subject.contentFor('head-barter'), '<meta name="sentry:revision"></meta>');
+      assert.notEqual(subject.contentFor('head-barter'), '<meta name="sentry:revision">');
     })
   });
 
